5f947014a5c6e0f06624be52ba07dfa2d1f11afa
Rufus <rufusponniah@icloud.com>
2024-12-25 20:44:51 +0000

    test solver with given sudoku files up to given permutation with & certainty

--- tester/tester.py
+++ tester/tester.py
@@ -1,23 +1,46 @@
 import subprocess;
 
-path = "./solver/solver"
+path = "./build/solver"
+make_command = ["make",  "-C", "./solver"]
 
-process = subprocess.Popen(
-    path,
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE, 
-    stderr=subprocess.PIPE, 
-    text=True 
-)
+permutation_width_max = 9
+permutation_certainty_max = 9
 
-process.wait()
+test_filepath = "./examples/test5.csv"
+solution_filepath = "./examples/test5solutions.csv"
+test_number = 100
+
+results = [None] * (permutation_width_max - 1) * (permutation_certainty_max - 1) 
+
+# compile solver
+make_process = subprocess.Popen(make_command , stderr=subprocess.STDOUT)
+make_process.wait()
+
+for i in range(1, permutation_certainty_max):
+    for y in range(1, permutation_width_max):
+
+
+        test_process = subprocess.Popen(
+            [path, str(i), str(y), test_filepath, solution_filepath, str(test_number)],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE, 
+            text=True 
+        )
+
+        test_process.wait()
+
+        if(test_process.returncode != 0):
+            print("test execution failed")
+            print(test_process.stdout.readline())
+        else: 
+            results[(i * (1 - permutation_width_max) + y)] = test_process.stdout.readline().split(' ')[:-1]
 
 # Close the streams
-output = process.stdout.readlines()  # Read one line of output
-print("Output:")
-for o in output:
-    print(o)
 
-process.stdin.close()
-process.stdout.close()
-process.stderr.close()
+print("Output:")
+print(results)
+
+test_process.stdin.close()
+test_process.stdout.close()
+test_process.stderr.close()
