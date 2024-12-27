2ffa22168819ac75f0d70f8b430a3358073e3eb9
Rufus <rufusponniah@icloud.com>
2024-12-27 00:21:10 +0000

    add exclusivity option for filter (remove provided indeces rather than retain provided indeces)

--- tester/example_filter/exampleFilter.py
+++ tester/example_filter/exampleFilter.py
@@ -1,4 +1,6 @@
 # this utility filters the puzzles in examples.csv to exclude puzzles at the indeces appearing in filter.csv
+keepFilter = False # true to keep all indeces in filter, false to remove all indeces in filter
+
 import os
 dir_path = "./tester/example_filter/"
 
@@ -9,9 +11,17 @@ with open(dir_path + "filter.csv", "r") as f:
     removeIndeces = f.read().split(",")
 
 
+rm = [ i for i in range(0, 10000)]
 
-for i in range(len(removeIndeces) - 1, 0, -1):
-    del examples[int(removeIndeces[i])]
+if keepFilter:
+    for i in range(len(removeIndeces) - 1, 0, -1):
+        del examples[int(removeIndeces[i])]
+else:
+    for i in range(len(removeIndeces) - 1, 0, -1):
+        del rm[int(removeIndeces[i])]
+
+    for i in range(len(rm) - 1, 0, -1):
+        del examples[int(rm[i])]
 
 with open(dir_path + "filteredExamples.csv", "w") as f:
     for e in examples:
