7844370d148b0e472fc569e39686e5e73ae3f518
Rufus <rufusponniah@icloud.com>
2024-12-31 19:31:17 +0000

    add minimum perm width and certainty

--- tester/tester.py
+++ tester/tester.py
@@ -5,25 +5,29 @@ import pandas as pd
 path = "./build/solver"
 make_command = ["make",  "-C", "./solver"]
 
-permutation_width_max = 10
-permutation_certainty_max = 10
+permutation_width_max = 9
+permutation_certainty_max = 9
+
+# minimum needed for testing solver with brute force feature. Width 1 would be solved entirely by brute force.
+permutation_width_min = 2
+permutation_certainty_min = 5
 
 test_filepath = "./examples/allUnsolveableTest.csv"
 solution_filepath = "./examples/allUnsolveableTestSolutions.csv"
-test_number = 5000
+test_number = 100
 
-results = [None] * (permutation_width_max - 1) * (permutation_certainty_max - 1) 
+results = [None] * (permutation_width_max - permutation_width_min) * (permutation_certainty_max - permutation_certainty_min) 
 
 # compile solver
 make_process = subprocess.Popen(make_command , stderr=subprocess.STDOUT)
 make_process.wait()
 
-for i in range(1, permutation_certainty_max):
-    for y in range(1, permutation_width_max):
+for i in range(permutation_certainty_max - permutation_certainty_min):
+    for y in range(permutation_width_max - permutation_width_min):
 
 
         test_process = subprocess.Popen(
-            [path, str(i), str(y), test_filepath, solution_filepath, str(test_number)],
+            [path, str(y + permutation_width_min), str(i + permutation_certainty_min), test_filepath, solution_filepath, str(test_number)],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE, 
             stderr=subprocess.PIPE, 
@@ -37,7 +41,7 @@ for i in range(1, permutation_certainty_max):
             print("test execution failed")
             print(test_process.stdout.readline())
         else: 
-            results[(i * (1 - permutation_width_max) + y)] = [int(r) for r in test_process.stdout.readline().split(' ')]
+            results[(i * (permutation_width_max - permutation_width_min) + y)] = [int(r) for r in test_process.stdout.readline().split(' ')]
 
 # Close the streams
 
@@ -50,8 +54,11 @@ test_process.stderr.close()
 
 
 df = pd.DataFrame(results, columns= ["width", "certainty", "successNo", "time"])
+df["time/puzzle"] = [t / test_number for t in df["time"]]
 
 df = df[df['successNo'] == float(df['successNo'].max())]
+
+
 
 df = df.sort_values(by=['time'])
 df.sort_index()
